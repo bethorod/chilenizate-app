96dc9e3761e165887b54f051688a01f98193d6de
Heberto Rodriguez <hebertoerodriguez@gmail.com>
2026-09-14 22:53:32 -0300

    feat: config to page Load juegos

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import netlify from "@netlify/vite-plugin";
 import path from "path";
 import { sites } from "@openai/sites-vite-plugin";
 
@@ -11,6 +12,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    netlify(),
     sites(),
   ],
   resolve: {
